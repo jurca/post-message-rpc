06f709d961e192528cccb7d1e6d4c4bbe21c2d8a
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-10-02 12:57:05 +0200

    improved test coverage to acceptable level

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -329,6 +329,135 @@ describe('createServer', () => {
     expect(reply2.error.message).toBe(`The procedure ${randomProcedure} is not provided by this server`)
   })
 
+  it('should report immediate and delayed procedure errors to the caller', async () => {
+    createServer(0, [], {
+      bar: () => Promise.reject(new SyntaxError('bar threw an error')),
+      foo: () => {
+        throw new TypeError('foo threw an error')
+      },
+    })
+    const serverListener = (addEventListener as any).calls[0][1]
+    const peer = {
+      postMessage: jest.fn(),
+    }
+    serverListener({
+      data: {
+        channel: 0,
+        data: {
+          arguments: [],
+          callId: 'callIdError1',
+          procedure: 'foo',
+        },
+        messageId: 'msg1d',
+      },
+      origin: '*',
+      source: peer,
+    })
+
+    // await the reply connection
+    while (peer.postMessage.mock.calls.length < 2) {
+      await Promise.resolve()
+    }
+    // accept reply connection handshake
+    globalMessageListener({
+      data: {
+        channel: 0,
+        messageId: peer.postMessage.mock.calls[1][0].messageId,
+        received: true,
+      },
+      origin: '*',
+      source: peer,
+    })
+
+    // await the reply message
+    while (peer.postMessage.mock.calls.length < 3) {
+      await Promise.resolve()
+    }
+    const reply1 = peer.postMessage.mock.calls[2][0].data
+    expect(reply1.callId).toBe('callIdError1')
+    expect(reply1.error.name).toBe('TypeError')
+    expect(reply1.error.message).toBe('foo threw an error')
+
+    serverListener({
+      data: {
+        channel: 0,
+        data: {
+          arguments: [],
+          callId: 'callIdError2',
+          procedure: 'bar',
+        },
+        messageId: 'msg1d0',
+      },
+      origin: '*',
+      source: peer,
+    })
+    // await the reply message
+    while (peer.postMessage.mock.calls.length < 5) {
+      await Promise.resolve()
+    }
+    const reply2 = peer.postMessage.mock.calls[4][0].data
+    expect(reply2.error.name).toBe('SyntaxError')
+    expect(reply2.error.message).toBe('bar threw an error')
+    expect(reply2.error.stack).toBeTruthy()
+  })
+
+  it('should log to the console failed attempts of sending a reply', async () => {
+    const nativeConsoleError = console.error // tslint:disable-line no-console
+    console.error = jest.fn() // tslint:disable-line no-console
+    createServer(0, [], {
+      foo: () => 1,
+    })
+    const serverListener = (addEventListener as any).calls[0][1]
+    const peer = {
+      postMessage: jest.fn(),
+    }
+    serverListener({
+      data: {
+        channel: 0,
+        data: {
+          arguments: [],
+          callId: 'callId',
+          procedure: 'foo',
+        },
+        messageId: 'msg1d',
+      },
+      origin: '*',
+      source: peer,
+    })
+
+    // await the reply connection
+    while (peer.postMessage.mock.calls.length < 2) {
+      await Promise.resolve()
+    }
+    // accept reply connection handshake
+    globalMessageListener({
+      data: {
+        channel: 0,
+        messageId: peer.postMessage.mock.calls[1][0].messageId,
+        received: true,
+      },
+      origin: '*',
+      source: peer,
+    })
+
+    // await the reply connection
+    while (peer.postMessage.mock.calls.length < 3) {
+      await Promise.resolve()
+    }
+
+    // trigger timeout at the server by not sending a message confirmation back
+    jest.advanceTimersByTime(10_000)
+
+    // await all the async callbacks to be triggered
+    await Promise.resolve().then(() => null).then(() => null).then(() => null)
+
+    expect(console.error).toHaveBeenCalledTimes(1) // tslint:disable-line no-console
+    expect((console.error as any).mock.calls[0][0]).toBe('RPC communication failed') // tslint:disable-line no-console
+    expect((console.error as any).mock.calls[0][1] instanceof Error).toBe(true) // tslint:disable-line no-console
+
+    console.error = nativeConsoleError // tslint:disable-line no-console
+  })
+
   afterEach(() => {
     (addEventListener as any).calls.splice(0)
     ;(postMessage as any).calls.splice(0) // tslint:disable-line align whitespace
